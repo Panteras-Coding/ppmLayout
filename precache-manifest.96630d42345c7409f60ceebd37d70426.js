80bdaa2cd87d3a93e41a3267ec5f59f6927788f8
MandoHdz305 <54876728+MandoHdz305@users.noreply.github.com>
2020-07-09 23:09:11 -0500

    Updates

---
+++ precache-manifest.96630d42345c7409f60ceebd37d70426.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebc9a1981233aa8438db396e5d8dffde",
+    "url": "/ppmLayout/index.html"
+  },
+  {
+    "revision": "52dbd1f766982fb9f32b",
+    "url": "/ppmLayout/static/css/main.2236ccbc.chunk.css"
+  },
+  {
+    "revision": "7ae81107d89616f2c1e7",
+    "url": "/ppmLayout/static/js/2.4369a458.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ppmLayout/static/js/2.4369a458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52dbd1f766982fb9f32b",
+    "url": "/ppmLayout/static/js/main.56931185.chunk.js"
+  },
+  {
+    "revision": "2586b0ca4572ac82fada",
+    "url": "/ppmLayout/static/js/runtime-main.eeb9f2f8.js"
+  },
+  {
+    "revision": "72a3a84e75feb047618a069d4192b41e",
+    "url": "/ppmLayout/static/media/LOGO_Blanco.72a3a84e.png"
+  },
+  {
+    "revision": "d1117e5b9e97c80ea17e173620410436",
+    "url": "/ppmLayout/static/media/calendar.d1117e5b.png"
+  },
+  {
+    "revision": "17d2e3260c3edb3aa758a991f96cb07a",
+    "url": "/ppmLayout/static/media/workplace.17d2e326.png"
+  }
+]);
