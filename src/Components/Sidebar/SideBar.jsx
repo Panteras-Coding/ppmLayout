f86a94dc323c83969af5df5e29ee28df2bc756b3
Gabriela Velázquez Mason <0202578@up.edu.mx>
2021-02-10 15:35:04 -0600

    Cambios

--- src/Components/Sidebar/SideBar.jsx
+++ src/Components/Sidebar/SideBar.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import MenuList from "./MenuList";
 import UserSpace from "./UserSpace";
 import InfoBox from "./InfoBox";
-import Footer from "./Footer";
-
 const SideBar = (props) => {
     let currentClassName = "sideBar sbOpened"
     if(props.status) {
@@ -20,7 +18,6 @@ const SideBar = (props) => {
                     notHome={props.notHome}
                 />
                 <InfoBox status={props.status}/>
-                <Footer/>
             </div>
 
         </div>
