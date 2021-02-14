d0e93c2b1c8232fba4f67b92361ec517d8d0b12e
Michter99 <miguelrichter99@hotmail.com>
2021-02-14 00:23:13 -0600

    First Login Version

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
 import React, {useState} from 'react';
 import SideBar from "./Components/Sidebar/SideBar";
 import Main from "./Components/Main/Main";
+import Login from "./Components/Login/Login";
 import {HashRouter as Router} from 'react-router-dom'
 
 const App = () =>{
+
+    const [isLoggedIn, setLogState] = useState(false);
+
     const [completeBar, setShowBar] = useState(false); //State hook to open or close sidebar
     const [inDashboard, changeView] = useState(true);
-
-    return (
-        <Router>
-            <div>
-                <SideBar
-                    change={setShowBar}
-                    status={completeBar}
-                    name={"Armando Hernández"}
-                    notHome={changeView}
-                />
-                <Main
-                    status={completeBar}
-                    name={"Armando Hernandez"}
-                    indash={inDashboard}
-                />
-            </div>
-        </Router>
-    );
+    
+    if (isLoggedIn === true)
+    {
+        return (
+            <Router>
+                <div>
+                    <SideBar
+                        change={setShowBar}
+                        status={completeBar}
+                        name={"Armando Hernández"}
+                        notHome={changeView}
+                    />
+                    <Main
+                        status={completeBar}
+                        name={"Armando Hernandez"}
+                        indash={inDashboard}
+                    />
+                </div>
+            </Router>
+        );
+    }
+    else{
+        return (
+            <Router>
+                <div>
+                    <Login changeLog={() => setLogState(true)}/>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
